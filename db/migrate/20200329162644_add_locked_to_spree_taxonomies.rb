206a46dba708d785066a570ba56b5bd45bdc256d
Eran Peer <eran.peer@medwing.com>
2020-03-29 19:13:44 +0200

    added rake task to make store locked

--- db/migrate/20200329162644_add_locked_to_spree_taxonomies.rb
+++ db/migrate/20200329162644_add_locked_to_spree_taxonomies.rb
@@ -1,5 +1,10 @@
 class AddLockedToSpreeTaxonomies < ActiveRecord::Migration[6.0]
-  def change
+  def up
     add_column :spree_taxonomies, :locked, :boolean, default: false
+    Rake::Task['lock_store_taxonomy'].invoke
+  end
+
+  def down
+    remove_column :spree_taxonomies, :locked
   end
 end
