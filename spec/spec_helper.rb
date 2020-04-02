73730b1059b9803ed634eead05445a05cf2f5698
Val <arkhipov.valentin@gmail.com>
2020-04-02 14:13:25 -0400

    DatabaseCleaner

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -95,4 +95,32 @@ RSpec.configure do |config|
   #   # test failures related to randomization by passing the same `--seed` value
   #   # as the one that triggered the failure.
   #   Kernel.srand config.seed
+
+  config.before(:suite) do
+    if config.use_transactional_fixtures?
+      raise(<<-MSG)
+        Delete line `config.use_transactional_fixtures = true` from rails_helper.rb
+        (or set it to false) to prevent uncommitted transactions being used in
+        JavaScript-dependent specs.
+
+        During testing, the app-under-test that the browser driver connects to
+        uses a different database connection to the database connection used by
+        the spec. The app's database connection would not be able to access
+        uncommitted transaction data setup over the spec's database connection.
+      MSG
+    end
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each, type: :feature) do
+    # :rack_test driver's Rack app under test shares database connection
+    # with the specs, so continue to use transaction strategy for speed.
+    driver_shares_db_connection_with_specs = Capybara.current_driver == :rack_test
+
+    DatabaseCleaner.strategy = :truncation unless driver_shares_db_connection_with_specs # specs, so use truncation strategy. # under test that does *not* share a database connection with the # Driver is probably for an external browser with an app
+  end
+
+  config.before(:each) { DatabaseCleaner.start }
+
+  config.append_after(:each) { DatabaseCleaner.clean }
 end
