572fe9a24b94e306e4b6dbb0a4ec72d097e95e49
Eran Peer <eran.peer@medwing.com>
2020-04-02 20:39:59 +0200

    fixed cloudinary

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,8 +1,11 @@
-if ENV['CLOUDINARY_URL']
+if ENV['CLOUDINARY_URL'] && ENV['CLOUDINARY_API_KEY'] && ENV['CLOUDINARY_API_SECRET']
   Cloudinary.config do |config|
     config.secure = true
     config.cdn_subdomain = true
+    config.cloud_name = 'images'
+    config.api_key = ENV['CLOUDINARY_API_KEY']
+    config.api_secret = ENV['CLOUDINARY_API_SECRET']
   end
 else
-  Rails.logger.info { puts "****** Warning: There is no 'cloudinary' configuration info" }
+  Rails.logger.info { puts '****** Warning: There is no 'cloudinary' configuration info' }
 end
