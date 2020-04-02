5a08eb6890ae3a6a69f7ed448c02a55887e521ed
Eran Peer <eran.peer@medwing.com>
2020-04-02 20:59:34 +0200

    attempt at fixing cloudinary config

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -2,10 +2,19 @@ if ENV['CLOUDINARY_URL'] && ENV['CLOUDINARY_API_KEY'] && ENV['CLOUDINARY_API_SEC
   Cloudinary.config do |config|
     config.secure = true
     config.cdn_subdomain = true
-    config.cloud_name = 'images'
     config.api_key = ENV['CLOUDINARY_API_KEY']
     config.api_secret = ENV['CLOUDINARY_API_SECRET']
   end
 else
   Rails.logger.info { puts "****** Warning: There is no 'cloudinary' configuration info" }
+end
+
+module Cloudinary::CarrierWave
+  def public_id
+    return(
+      "#{Rails.application.class.parent_name.downcase}/#{
+        File.basename(@filename.downcase, File.extname(@filename.downcase))
+      }-#{Cloudinary::Utils.random_public_id}"
+    )
+  end
 end
