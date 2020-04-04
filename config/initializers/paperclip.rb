21130b05cc4c49229acf1996b02fae0405594a99
Eran Peer <eran.peer@medwing.com>
2020-04-04 17:24:50 +0200

    fixed environment variable

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -9,7 +9,7 @@ if ENV['BUCKETEER_AWS_ACCESS_KEY_ID']
       aws_secret_access_key: ENV['BUCKETEER_AWS_SECRET_ACCESS_KEY'],
       region: ENV['BUCKETEER_AWS_REGION']
     },
-    fog_directory: ENV['bucketeer-e239f3a0-75e3-4b15-974e-616f22f0c45e']
+    fog_directory: ENV['BUCKETEER_BUCKET_NAME']
   )
 
   Spree::Image.attachment_definitions[:attachment].delete(:url)
