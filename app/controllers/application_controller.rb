5ea491c0863ff94f564d5e2ee8f40ab110f688cf
Penny (Jitera) <developers@jitera.com>
2023-11-14 23:44:16 +0000

    feat[business-logic]: implement 'Confirm user email' Changes * '/app/controllers/application_controller.rb': + Create a new method `confirm_email` in the ApplicationController. This method will be responsible for handling the email confirmation process..+ The `confirm_email` method should first validate the input data. It should check if the email is not empty and if it is in the correct format. You can us...

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,25 @@
 # frozen_string_literal: true
 class ApplicationController < ActionController::Base
+  def confirm_email
+    # Validate the email
+    email = params[:email]
+    if email.blank? || !(email =~ URI::MailTo::EMAIL_REGEXP)
+      flash[:error] = "Invalid email address"
+      return
+    end
+    # Send confirmation email
+    confirmation_token = SecureRandom.urlsafe_base64
+    user = User.find_by_email(email)
+    if user
+      user.update(confirmation_token: confirmation_token)
+      ApplicationMailer.confirmation_email(user).deliver_now
+      flash[:notice] = "Confirmation email has been sent. Please check your email."
+    else
+      flash[:error] = "User not found"
+    end
+    # Wait for user to click on the confirmation link
+    # This will be handled in another action, e.g., UsersController#confirm_email
+  end
   def create_user(username, password)
     # Validate input
     return unless username.present? && password.present?
