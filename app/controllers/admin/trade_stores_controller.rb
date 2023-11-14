2c091b9d20e99bb1a0555441ea36ebc8f5d96138
Penelope (Jitera) <developers@jitera.com>
2023-11-14 23:44:24 +0000

    feat[business-logic]: implement 'Update shop information' Changes * '/app/controllers/admin/trade_stores_controller.rb': + Write a new method `update_store` in the `TradeStoresController` class. This method will receive the parameters "id", "name", and "address"..+ Use the `find_by` method to find the store with the provided ID. If the store does not exist, return an error message..+ If the store exists, use the `upda...

--- app/controllers/admin/trade_stores_controller.rb
+++ app/controllers/admin/trade_stores_controller.rb
@@ -1,19 +1,15 @@
 # frozen_string_literal: true
-
 module Admin
   class TradeStoresController < Spree::Admin::ResourceController
     def index
       @stores =
         ::Store.page(params[:page]).per(params[:per_page] || Spree::Config[:orders_per_page])
     end
-
     def new
       @store = ::Store.new
     end
-
     def create
       @store = ::Store.new(store_params)
-
       if @store.save
         redirect_to admin_trade_stores_url, notice: t('.created_successfully')
       else
@@ -21,14 +17,11 @@ module Admin
         render :new, status: :unprocessable_entity
       end
     end
-
     def edit
       @store = ::Store.find(params[:id])
     end
-
     def update
       @store = ::Store.find(params[:id])
-
       if @store.update(store_params)
         redirect_to edit_admin_trade_store_url(@store), notice: t('.updated')
       else
@@ -36,13 +29,24 @@ module Admin
         render :edit, status: :unprocessable_entity
       end
     end
-
+    def update_store
+      @store = ::Store.find_by(id: params[:id])
+      if @store.nil?
+        render json: { error: 'Store not found' }, status: :not_found
+      else
+        if @store.update(name: params[:name], address: params[:address])
+          render json: { message: 'Store updated successfully' }, status: :ok
+        else
+          render json: { error: @store.errors.full_messages.join(', ') }, status: :unprocessable_entity
+        end
+      end
+    rescue StandardError => e
+      render json: { error: e.message }, status: :internal_server_error
+    end
     private
-
     def model_class
       ::Store
     end
-
     # rubocop:disable Metrics/MethodLength
     def store_params
       params.require(:store).permit(
@@ -57,7 +61,6 @@ module Admin
         :is_hub
       )
     end
-
     # rubocop:enable Metrics/MethodLength
   end
 end
