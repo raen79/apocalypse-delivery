372a654e93cecd3db612d69d4394e8f09b87a9ce
Magdalena (Jitera) <developers@jitera.com>
2023-11-14 23:43:55 +0000

    feat[business-logic]: implement 'Create user' Changes * '/app/controllers/application_controller.rb': + Create a new method `create_user` in the ApplicationController. This method will handle the creation of a new user..+ The `create_user` method should accept two parameters: `username` and `password`..+ Use ActiveRecord's `create` method to create a new user. This method should be called on the Use...

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 # frozen_string_literal: true
-
-class ApplicationController < ActionController::Base; end
+class ApplicationController < ActionController::Base
+  def create_user(username, password)
+    # Validate input
+    return unless username.present? && password.present?
+    # Check if username is already taken
+    user = User.find_by(username: username)
+    return if user.present?
+    # Encrypt password
+    password_digest = BCrypt::Password.create(password)
+    # Create new user
+    new_user = User.create(username: username, password_digest: password_digest)
+    # Return new user ID
+    new_user.id
+  end
+end
